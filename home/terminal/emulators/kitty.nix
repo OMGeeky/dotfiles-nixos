ffc8196b1898ef35871c7ae8ad045e5ad8c1b179
OMGeeky <aalaalgmx@gmail.com>
2024-11-29 23:12:50 +0100

    change kitty theming

--- home/terminal/emulators/kitty.nix
+++ home/terminal/emulators/kitty.nix
@@ -1,28 +1,30 @@
 {
-  programs.kitty = {
-    enable = true;
-    font = {
-      size = 10;
-      name = "JetBrains Mono";
-    };
+    programs.kitty = {
+        enable = true;
+        font = {
+            size = 10;
+            name = "JetBrains Mono";
+        };
 
-    settings = {
-      scrollback_lines = 10000;
-      placement_strategy = "center";
-
-      allow_remote_control = "yes";
-      enable_audio_bell = "no";
+        settings = {
+            confirm_os_window_close = 0;
+            scrollback_lines = 10000;
+            placement_strategy = "center";
+    
+            allow_remote_control = "yes";
+            enable_audio_bell = "no";
 #      visual_bell_duration = "0.1";
 
-      copy_on_select = "clipboard";
+            copy_on_select = "clipboard";
 
-      selection_foreground = "none";
-      selection_background = "none";
+            selection_foreground = "none";
+            selection_background = "none";
 
-      # colors
-      background_opacity = "0.7";
+            # colors
+            background_opacity = "0.3";
+            background_blur = 0;
+        };
+
+        themeFile = "Catppuccin-Mocha";
     };
-
-#    theme = "Catppuccin-Mocha";
-  };
 }
