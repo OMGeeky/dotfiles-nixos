63d62bcb04e9bd9a7d905d0359ff5ee6856ae76c
OMGeeky <aalaalgmx@gmail.com>
2025-01-26 13:26:45 +0100

    idk, unity still doesnt like my window rules

--- home/progs/wayland/hyprland/rules.nix
+++ home/progs/wayland/hyprland/rules.nix
@@ -71,7 +71,7 @@
       "size 640 400, class:^(.*jetbrains.*)$, title:^(splash)$"
 
 
-      "minsize 300 300, initialClass:(Unity) !(title:^(UnityTooltipWindow)$)"
+      "minsize 300 300, class:(Unity) !(initialTitle:(UnityTooltipWindow))" 
     ];
   };
 }
