2b302052c6f397923ca0e204bfb8d0f17d2cf761
OMGeeky <aalaalgmx@gmail.com>
2025-01-28 20:20:57 +0100

    change eww config stuff

--- home/progs/wayland/eww.nix
+++ home/progs/wayland/eww.nix
@@ -1,6 +1,15 @@
-{ ... }:{ 
+{pkgs, ... }:{ 
     programs.eww = {
         enable = true;
         configDir = ./config/eww;
     };
+    home.packages = with pkgs;[
+        bash
+        gawk # gnu implementation of awk
+        coreutils # should include stdbuf & seq
+        gnugrep # gnu implementation of grep
+        socat
+        jq
+        python3
+    ];
 }
