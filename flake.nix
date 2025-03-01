2d25c8da3a70de73e460ce97a521f088ee3de673
OMGeeky <aalaalgmx@gmail.com>
2025-03-01 16:17:29 +0100

    add cachix (hyprland & nix-community)

--- flake.nix
+++ flake.nix
@@ -1,6 +1,16 @@
 {
     description="A flake by the book";
 
+    nixConfig = {
+        extra-substituters = [
+            "https://hyprland.cachix.org"
+            "https://nix-community.cachix.org"
+        ];
+        extra-trusted-public-keys = [
+            "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        ];
+    };
 
     outputs = inputs: 
         inputs.flake-parts.lib.mkFlake {inherit inputs;} {
@@ -56,7 +66,7 @@
         # hyprwm
         hyprland = {
           url = "github:hyprwm/hyprland";
-          inputs.nixpkgs.follows = "nixpkgs";
+#          inputs.nixpkgs.follows = "nixpkgs";
         };
     
         hypridle = {
