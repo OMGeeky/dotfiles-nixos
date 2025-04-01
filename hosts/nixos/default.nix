3c965fc390f1b05878705ec77a95418f668dd878
OMGeeky <aalaalgmx@gmail.com>
2025-04-01 20:50:25 +0200

    add firewall-rule

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -3,6 +3,12 @@
         ./nvidia-bus-id.nix
         ./hardware-configuration.nix
     ];
-    networking.hostName = "nixos";
+    networking = {
+        hostName = "nixos";
+        firewall = {
+            enable = true;
+            allowedTCPPorts = [ 54321 ];
+        };
+    };
     security.tpm2.enable = true;
 }
