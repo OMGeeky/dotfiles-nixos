f987717fed49f531a2c750a153b55e9c30c2acc4
OMGeeky <aalaalgmx@gmail.com>
2025-04-18 13:49:11 +0200

    add networkmanager

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -5,6 +5,7 @@
     ];
     networking = {
         hostName = "nixos";
+        networkmanager.enable = true;
         firewall = {
             enable = true;
             allowedTCPPorts = [ 54321 ];
