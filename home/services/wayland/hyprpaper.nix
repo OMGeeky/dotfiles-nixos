40f6c876726a71460f408f74a34c1993850db53a
OMGeeky <aalaalgmx@gmail.com>
2025-07-28 15:28:50 +0200

    change wallpaper switching service to correctly start after login

--- home/services/wayland/hyprpaper.nix
+++ home/services/wayland/hyprpaper.nix
@@ -9,6 +9,20 @@
 let
   wallpaperDir = "${self}/submods/nixos-media/wallpapers"; # Replace with your wallpaper folder path
   wallpaperScript = pkgs.writeShellScriptBin "hyprland-wallpaper-switcher" ''
+    # Wait up to 10 seconds for the hyprpaper socket to appear
+    for i in {1..10}; do
+        if [ -e "$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.hyprpaper.sock" ]; then
+            break
+        fi
+        sleep 1
+    done
+
+    # Check if the socket is still missing after the loop
+    if [ ! -e "$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.hyprpaper.sock" ]; then
+        echo "Hyprpaper socket not found after waiting."
+        exit 1
+    fi
+
     WALLPAPER_DIR="${wallpaperDir}"
 
     CURRENT_WALLPAPER=$(hyprctl hyprpaper listloaded)
@@ -38,8 +52,8 @@ in
   systemd.user.services.hyprland-wallpaper-switcher = {
     Unit = {
       Description = "Hyprland Wallpaper Switcher";
-      After = ["hyprpaper.service"];
-      Requires = ["hyprpaper.service"];
+      After = ["graphical-session.target" "hyprpaper.service"];
+      Requires = ["graphical-session.target" "hyprpaper.service"];
     };
     Service = {
       Type = "oneshot";
@@ -56,12 +70,12 @@ in
       Description = "Hyprland Wallpaper Switcher Timer";
     };
     Timer = {
-      OnBootSec = "10"; # Start after 10 seconds on boot
+      OnActiveSec = "10"; # Start after 10 seconds on boot
       OnUnitActiveSec = "10min"; # Run every 10 minutes
       Unit = "hyprland-wallpaper-switcher.service";
     };
     Install = {
-      WantedBy = ["timers.target"];
+#      WantedBy = ["graphical-session.target"];
     };
   };
 
