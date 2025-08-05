7f4359830502d42b2345773eb0ce356713253f68
OMGeeky <aalaalgmx@gmail.com>
2025-08-05 16:17:41 +0200

    extract rust-tools option into own file

--- home/options/default.nix
+++ home/options/default.nix
@@ -1,41 +1,6 @@
 { config, lib, pkgs, ... }: {
+    imports = [
+        ./rust-tools.nix
+    ];
     # specify configs
-    options.rust-tools = {
-        enable = lib.mkEnableOption "Enable some Rust tools";
-        packages = lib.mkOption {
-            type = lib.types.listOf lib.types.package;
-            default = with pkgs; [
-                rustup
-                gcc
-                rustc
-                openssl
-#                cargo
-#                rustfmt
-#                clippy
-#                rust-analyzer
-            ];
-        };
-
-        # Add option to specify default Rust edition
-        defaultEdition = lib.mkOption {
-            type = lib.types.str;
-            default = "2024";
-            description = "Default Rust edition to use";
-        };
-    };
-
-    # specify what happens with the configs
-    config = lib.mkIf config.rust-tools.enable {
-        home.packages = config.rust-tools.packages;
-        # Set the default Rust edition in the shell environment
-        home.shellAliases = {
-            cargo-generate = "cargo generate --edition \${RUST_EDITION}";
-        };
-
-        home.sessionVariables = {
-            RUST_EDITION = config.rust-tools.defaultEdition;
-        };
-
-#        home = import ../progs/jetbrains/rust-rover.nix;
-    };    
 }
---
+++ home/options/rust-tools.nix
@@ -0,0 +1,44 @@
+{config, lib, pkgs,...}:
+{
+    options.rust-tools = {
+        enable = lib.mkEnableOption "Enable some Rust tools";
+        packages = lib.mkOption {
+            type = lib.types.listOf lib.types.package;
+            default = with pkgs; [
+                rustup
+                gcc
+                rustc
+                openssl
+                pkg-config
+#                cargo
+#                rustfmt
+#                clippy
+#                rust-analyzer
+            ];
+        };
+
+        # Add option to specify default Rust edition
+        defaultEdition = lib.mkOption {
+            type = lib.types.str;
+            default = "2024";
+            description = "Default Rust edition to use";
+        };
+    };
+
+    # specify what happens with the configs
+    config = lib.mkIf config.rust-tools.enable {
+        home.packages = config.rust-tools.packages;
+        # Set the default Rust edition in the shell environment
+        home.shellAliases = {
+            cargo-generate = "cargo generate --edition \${RUST_EDITION}";
+        };
+
+        home.sessionVariables = {
+            RUST_EDITION = config.rust-tools.defaultEdition;
+	    PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+        };
+
+#        home = import ../progs/jetbrains/rust-rover.nix;
+    };    
+
+}
