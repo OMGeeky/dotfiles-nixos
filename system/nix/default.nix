ad9c37489106b46beed4b2fc7525859d6d529850
OMGeeky <aalaalgmx@gmail.com>
2025-08-05 21:47:51 +0200

    re-format system/nix/default.nix & add make and unzip

--- system/nix/default.nix
+++ system/nix/default.nix
@@ -1,68 +1,65 @@
+{ config, pkgs, inputs, lib, ... }:
 {
-    config,
-    pkgs,
-    inputs,
-    lib,
-    ...
-}: 
-{
-    imports = [
-        ./nh.nix
-        ./nix-ld.nix
-        ./nixpkgs.nix
-        ./substituters.nix
-    ];
-    nixld.enable = true;
+	imports = [
+		./nh.nix
+		./nix-ld.nix
+		./nixpkgs.nix
+		./substituters.nix
+	];
+	nixld.enable = true;
 
-      # we need git for flakes
-    environment.systemPackages = with pkgs; [
-        git
-        kdePackages.dolphin
-        kdePackages.qtwayland
-        kdePackages.qtsvg
+	# we need git for flakes
+	environment.systemPackages = with pkgs; [
+		git
+		kdePackages.dolphin
+		kdePackages.qtwayland
+		kdePackages.qtsvg
 
-	# useful tools
-	psmisc # ex. killall, fuser
+		gnumake
+		unzip
 
-	usbutils
-	pciutils
+		# useful tools
+		psmisc # ex. killall, fuser
 
-	ripgrep # (rg) alternative to grep respecting .gitignore
-	fd # alternative to find
+		usbutils
+		pciutils
 
-	mtr # network diagnostic tool combining traceroute & ping
-	nmap # more network tooling
+		ripgrep # (rg) alternative to grep respecting .gitignore
+		fd # alternative to find
 
-	# monitoring
-	htop
-	iotop
+		mtr # network diagnostic tool combining traceroute & ping
+		nmap # more network tooling
 
-    ];
-    
-    virtualisation.docker.enable = true;
+		# monitoring
+		htop
+		iotop
 
-    nix = let
-        flakeInputs = lib.filterAttrs (_: v: lib.isType "flake" v) inputs;
-    in {
-        package = pkgs.lix;
-    
-        # pin the registry to avoid downloading and evaling a new nixpkgs version every time
-        registry = lib.mapAttrs (_: v: {flake = v;}) flakeInputs;
-    
-        # set the path for channels compat
-        nixPath = lib.mapAttrsToList (key: _: "${key}=flake:${key}") config.nix.registry;
-    
-        settings = {
-          auto-optimise-store = true;
-              builders-use-substitutes = true;
-          experimental-features = ["nix-command" "flakes"];
-          flake-registry = "/etc/nix/registry.json";
-    
-          # for direnv GC roots
-          keep-derivations = true;
-          keep-outputs = true;
-    
-          trusted-users = ["root" "@wheel"];
-        };
-    };
+	];
+
+	virtualisation.docker.enable = true;
+
+	nix = let
+		flakeInputs = lib.filterAttrs (_: v: lib.isType "flake" v) inputs;
+	in {
+		package = pkgs.lix;
+
+		# pin the registry to avoid downloading and evaling a new nixpkgs version every time
+		registry = lib.mapAttrs (_: v: {flake = v;}) flakeInputs;
+
+		# set the path for channels compat
+		nixPath = lib.mapAttrsToList (key: _: "${key}=flake:${key}") config.nix.registry;
+
+		settings = {
+			auto-optimise-store = true;
+			builders-use-substitutes = true;
+			experimental-features = ["nix-command" "flakes"];
+			flake-registry = "/etc/nix/registry.json";
+
+			# for direnv GC roots
+			keep-derivations = true;
+			keep-outputs = true;
+
+			trusted-users = ["root" "@wheel"];
+		};
+	};
 }
