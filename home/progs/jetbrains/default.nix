6eecb7c9fcec94dc67709af6fb002bad1ace85d7
OMGeeky <aalaalgmx@gmail.com>
2025-08-06 14:09:01 +0200

    format home/progs/jetbrains/default.nix & add session path for toolbox installed programs

--- home/progs/jetbrains/default.nix
+++ home/progs/jetbrains/default.nix
@@ -1,6 +1,11 @@
-{ pkgs, ... }: {
-    home.packages = with pkgs; [
-        android-studio 
-        jetbrains-toolbox
-    ];
+{ pkgs, ... }:
+{
+		home.packages = with pkgs; [
+				android-studio
+				jetbrains-toolbox
+		];
+		home.sessionPath = [
+				"~/.local/share/JetBrains/Toolbox/scripts"
+		];
+
 }
